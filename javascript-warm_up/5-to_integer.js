0ffe3c67a08f4e03fea3ad2e3c6bf62ca42ec5c4
Mhlaba Mkhatshane <hlabisto28@gmail.com>
2025-07-15 11:34:39 +0000

    corrected

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const num = process.argv[2];
 const convertedNum = parseInt(num);
 
 if (isNaN(convertedNum)) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
-  console.log("My number:", convertedNum);
+  console.log('My number:', convertedNum);
 }
