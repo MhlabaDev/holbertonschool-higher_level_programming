4a520e46df1d36aa0cceecb343ba1d77fb8e2e3c
MhlabaDev <hlabisto28@gmail.com>
2025-07-15 14:02:52 +0200

    Update 9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -4,10 +4,8 @@
 function add (a, b) {
   return a + b;
 }
-
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
-
 if (process.argv.length < 4) {
   console.log("NaN");
 } else {
